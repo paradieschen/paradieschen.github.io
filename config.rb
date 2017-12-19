e79797a864d8e7dd9250f5889b66af2e1066d7cb
paradieschen <35890026+paradieschen@users.noreply.github.com>
2017-12-19 19:07:05 +0100

    Middleman init

---
+++ config.rb
@@ -0,0 +1,31 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+set :haml, { :format => :html5 }
